64a865c5dd5e4689dee47c2501efa84c82a31481
Leandro Solidum <leo.g.solidum@gmail.com>
2025-01-28 13:03:42 -0800

    add solid because my last name is solidum

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 
+import solidJs from "@astrojs/solid-js";
+
+
 // https://astro.build/config
 export default defineConfig({
   vite: {    
       plugins: [tailwindcss()],  
   },
 
-  integrations: [],
+  integrations: [solidJs()],
 });
